8d8edfa11400caeb70f0cec15ece254e205ccdc3
Kahle <kahlkn@gmail.com>
2020-12-23 22:58:36 +0800

    Add application entry

---
+++ src/main/java/store/code/Application.java
@@ -0,0 +1,16 @@
+package store.code;
+
+import artoria.logging.Logger;
+import artoria.logging.LoggerFactory;
+
+/**
+ * Application entry.
+ * @author Kahle
+ */
+public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/store/code/ApplicationTest.java
@@ -0,0 +1,11 @@
+package store.code;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void test1() {
+    }
+
+}
