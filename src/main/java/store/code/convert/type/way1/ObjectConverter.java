b64fad11760f1b1b5827e5f4c31ad973e7c964b3
Kahle <kahlkn@gmail.com>
2021-06-23 22:41:11 +0800

    Add type conversion tools

---
+++ src/main/java/store/code/convert/type/way1/ObjectConverter.java
@@ -0,0 +1,32 @@
+package store.code.convert.type.way1;
+
+import artoria.util.Assert;
+import artoria.util.ClassUtils;
+
+/**
+ * Simple converter, so is mapping object.
+ * @author Kahle
+ */
+public class ObjectConverter implements TypeConverter {
+
+    @Override
+    public Object convert(Object source, Class<?> target) {
+        Assert.notNull(source, "Parameter \"source\" must not null. ");
+        Assert.notNull(target, "Parameter \"target\" must not null. ");
+        Class<?> clazz = source.getClass();
+        target = ClassUtils.getWrapper(target);
+        clazz = ClassUtils.getWrapper(clazz);
+        if (target.isAssignableFrom(clazz)) {
+            return source;
+        }
+        boolean tgIsStr = String.class.isAssignableFrom(target);
+        if (Boolean.class.isAssignableFrom(clazz) && tgIsStr) {
+            return source.toString();
+        }
+        if (Character.class.isAssignableFrom(clazz) && tgIsStr) {
+            return source.toString();
+        }
+        return source;
+    }
+
+}
