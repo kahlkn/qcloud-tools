2f18693458cba047122e321c4cdd247f3a0d8adf
Kahle <kahle@undefined>
2022-06-26 16:34:08 +0800

    Add SMS related tools

---
+++ src/main/java/store/code/message/way3/SmsSendResult.java
@@ -0,0 +1,55 @@
+package store.code.message.way3;
+
+import artoria.data.AbstractExtraData;
+
+import java.io.Serializable;
+
+/**
+ * Short messaging service send result.
+ * @author Kahle
+ */
+public class SmsSendResult extends AbstractExtraData implements Serializable {
+    /**
+     * Error code.
+     */
+    private String code;
+    /**
+     * Description of code.
+     */
+    private String description;
+    /**
+     * Business id.
+     */
+    private String businessId;
+
+    public String getCode() {
+
+        return code;
+    }
+
+    public void setCode(String code) {
+
+        this.code = code;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public void setDescription(String description) {
+
+        this.description = description;
+    }
+
+    public String getBusinessId() {
+
+        return businessId;
+    }
+
+    public void setBusinessId(String businessId) {
+
+        this.businessId = businessId;
+    }
+
+}
